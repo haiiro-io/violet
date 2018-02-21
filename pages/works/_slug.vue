7446a7806ee6f1e01947fd9ea2e5d5c32b369b64
hmsk <k.hamasaki@gmail.com>
2018-02-20 23:43:23 -0800

    Add page for /works/:slug

---
+++ pages/works/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    here's {{$route.params.slug}} work
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "nuxt-class-component";
+
+  @Component
+  export default class PageSelectedWork extends Vue {
+  }
+</script>
+
+<style lang="postcss">
+</style>
