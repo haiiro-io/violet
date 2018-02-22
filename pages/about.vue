b4d7782f3514cf966027570625271ffe4dacd432
hmsk <k.hamasaki@gmail.com>
2018-02-22 02:06:13 -0800

    Add /about

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <p>About</p>
+  </section>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "nuxt-class-component";
+
+  @Component
+  export default class PageAbout extends Vue {
+  }
+</script>
+
+<style lang="postcss">
+</style>
