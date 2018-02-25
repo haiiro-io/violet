376eae230df1e66bf7af25450401b150a27a4060
hmsk <k.hamasaki@gmail.com>
2018-02-24 20:07:51 -0800

    Import all md files dynamically

--- store/modules/works.ts
+++ store/modules/works.ts
@@ -3,12 +3,22 @@ import { RootState } from "store";
 
 import Work from "../../lib/work";
 
-import signifiant from "~/contents/works/en/signifiant.md";
-import laughly from "~/contents/works/en/laughly.md";
-const importedFrontMatters: { [name: string]: FrontMatterContent } = {
-  signifiant,
-  laughly
-}
+declare const require: any;
+
+type LANG = "en" | "ja";
+const LANGS: LANG[] = ["en", "ja"];
+type ImportedFrontMatters = { [name: string]: FrontMatterContent };
+const importsByLang: {
+  [lang: string]: ImportedFrontMatters
+} = { en: {}, ja: {} };
+
+const importAll = (resolve) => {
+  resolve.keys().forEach((key) => {
+    const [_, lang, name] = key.match(/^\.\/(..)\/(.+)\.md$/);
+    importsByLang[lang][name] = resolve(key);
+  });
+};
+importAll(require.context("~/contents/works", true, /\.md$/));
 
 export const name = "works";
 
@@ -17,18 +27,20 @@ export const types = {
 };
 
 export interface State {
-  objects: Work[];
+  en: Work[];
+  ja: Work[];
 }
 
 export const namespaced = true;
 
 export const state = (): State => ({
-  objects: []
+  en: [],
+  ja: []
 });
 
 export const getters: GetterTree<State, RootState> = {
   pick: (state) => (name: string): Work | undefined => {
-    return state.objects.find((work) => {
+    return state.en.find((work) => {
       return work.name === name;
     });
   }
@@ -38,20 +50,27 @@ export interface Actions<S, R> extends ActionTree<S, R> {
   initializeWorksFromAttributes (S, attributes): void;
 }
 
+const workFromFrontMatter = (name, frontmatter) => {
+  const attr = frontmatter.attributes;
+  attr.name = name;
+  attr.body = frontmatter.body;
+  return new Work(attr);
+};
+
 export const actions: Actions<State, RootState> = {
   initializeWorksFromAttributes ({ commit }) {
-    commit(types.INITIALIZE, Object.keys(importedFrontMatters).map(key => {
-      const frontmatter = importedFrontMatters[key];
-      const attr = frontmatter.attributes;
-      attr.name = key;
-      attr.body = frontmatter.body;
-      return new Work(attr);
-    }));
+    LANGS.forEach((lang) => {
+      const works = Object.keys(importsByLang[lang]).map(key => workFromFrontMatter(key, importsByLang.en[key]));
+      commit(
+        types.INITIALIZE,
+        { works, lang }
+      );
+    });
   }
 };
 
 export const mutations: MutationTree<State> = {
-  [types.INITIALIZE](state, works: Work[]) {
-    state.objects = works;
+  [types.INITIALIZE](state, payload :{ works: Work[], lang: string }) {
+    state[payload.lang] = payload.works;
   }
 };
