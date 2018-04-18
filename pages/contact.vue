77c10de15af76772366789a992d6ed3c92053f07
hmsk <k.hamasaki@gmail.com>
2018-04-18 03:02:02 -0700

    Create base contact page

---
+++ pages/contact.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="contact">
+    <h1>
+      Contact Me
+      <span class="workSelected-year">Say hello. Reaching out to me ☺</span>
+    </h1>
+    <form>
+      <haiiro-button>
+        Send
+      </haiiro-button>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+  import VueI18n from "vue-i18n";
+  import Component from "nuxt-class-component";
+
+  import PageBase from "~/lib/page-base";
+  import HaiiroButton from "~/components/HaiiroButton.vue";
+
+  @Component({
+    components: { HaiiroButton }
+  })
+  export default class PageContact extends PageBase {
+  }
+</script>
+
+<style lang="postcss" scoped>
+  @import "~/assets/styles/custom-properties.postcss";
+
+  section.contact {
+    @media (--narrow) {
+      padding-top: 170px;
+    }
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 48px;
+    line-height: 1.7;
+    font-weight: bold;
+    margin: 120px auto 120px;
+    color: var(--konezumi);
+    & > .workSelected-year {
+      font-size: 18px;
+      font-weight: normal;
+      line-height: 1.7;
+      display: block;
+      margin-top: 20px;
+      text-align: center;
+      color: var(--nibihai);
+    }
+  }
+
+  form {
+    display: flex;
+    justify-content: center;
+  }
+
+  .haiiroButton {
+    margin: 0 auto;
+    min-width: 240px;
+    text-align: center;
+  }
+
+</style>
