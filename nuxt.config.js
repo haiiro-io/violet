08661b1501b2078179a4523c513ad191f0db1027
hmsk <k.hamasaki@gmail.com>
2019-01-06 22:33:52 -0800

    Update regexp which is replaced with svg-sprite-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ module.exports = {
   build: {
     extend (config, { isDev, isClient }) {
       // remove existing url-loader settings once, for giving svg specific loader
-      const rule = config.module.rules.find(r => r.test.toString() === '/\\.(png|jpe?g|gif|svg)$/');
+      const rule = config.module.rules.find(r => r.test.toString() === '/\\.(png|jpe?g|gif|svg|webp)$/');
       config.module.rules.splice(config.module.rules.indexOf(rule), 1);
 
       config.module.rules.push({
